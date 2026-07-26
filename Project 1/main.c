ff0cf556bd5bd19b0272e5012b9902cb047bdf9a
prasad-rpatil <prasad12621@gmail.com>
2026-07-26 17:46:00 +0530

    Add files via upload

---
+++ Project 1/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int randomNumber;
+
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    randomNumber = (rand() % 100) + 1;
+    printf("Random Number: %d\n", randomNumber);
+    int no_of_guesses = 0;
+    int guessed;
+
+    do
+    {
+        printf("Guess the number\n");
+        scanf("%d", &guessed);
+        if(guessed > randomNumber){
+            printf("Lower number please\n");
+        }
+        else if(guessed < randomNumber){
+            printf("Higher number please\n");
+        }
+        no_of_guesses++;
+    } while (guessed != randomNumber);
+
+    printf("You guessed the number in %d guesses\n", no_of_guesses);
+
+    // printf("Random Number: %d\n", randomNumber);
+
+    return 0;
+}
